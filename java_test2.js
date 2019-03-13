550dcd45068217601eeb75fbb515b96d8e969da8
Matthew B <mjtb87@yahoo.com>
2019-03-13 09:41:39 -0600

    add java add ajax files

---
+++ java_test2.js
@@ -0,0 +1,30 @@
+<script>
+
+function myFunction() {
+	var num1 = 2;
+    var num2 = 12;
+    var equality = (num1 == "2");
+    var strict = (num1 === "2");
+    document.write("Equality: "+equality+"<br>");
+    document.write("Strict: "+strict+"<br><br>");
+    
+    document.write("Date type: "+typeof num1);
+    document.write("<br>Data type: "+typeof "2");
+    
+    var test = isNaN(num2);
+    console.log("isNan(num2): "+test);
+    var words = "words";
+    
+    var array = [1,2,3,4,5]
+    document.write("<br>Date type: "typeof array);
+    
+    var test2 = inNaN(words);
+    console.log("isNaN(test2): "+test2);
+    var results = num1 + num2;
+    document.write("<br>Data type: "+typeof results)
+    
+    console.log(num1+" + "+num2+" = "+results);
+};
+myFunction()
+
+</script>
